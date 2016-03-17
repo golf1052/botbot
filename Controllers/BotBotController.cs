9503ba50fdd12c4b03f846dfaea7de6321076f36
Sanders Lauture <golf1052@msn.com>
2016-03-16 21:17:54 -0400

    console -> webserver
    Because it crashed all the time, hopefully it's better.

---
+++ Controllers/BotBotController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace botbot.Controllers
+{
+    [Route("api/[controller]")]
+    public class BotBotController : Controller
+    {
+        public static Client client;
+        
+        public static async Task StartClient()
+        {
+            BotBotController.client = new Client();
+            HttpClient webClient = new HttpClient();
+            Uri uri = new Uri("https://slack.com/api/rtm.start?token=" + Secrets.Token);
+            HttpResponseMessage response = await webClient.GetAsync(uri);
+            JObject responseObject = JObject.Parse(await response.Content.ReadAsStringAsync());
+            await client.Connect(new Uri((string)responseObject["url"]));
+        }
+    }
+}
