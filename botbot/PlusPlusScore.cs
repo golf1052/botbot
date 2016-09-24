0d56d01c3bf9c74953d55737abea3a3dd4db8bc2
Sanders Lauture <golf1052@msn.com>
2016-09-24 12:43:20 -0400

    The computer switching

---
+++ botbot/PlusPlusScore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace botbot
+{
+    public class PlusPlusScore
+    {
+        public static PlusPlusThing GetThing(string thing)
+        {
+        }
+    }
+}
---
+++ botbot/PlusPlusThing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace botbot
+{
+    public class PlusPlusThing
+    {
+        public string Id { get; set; }
+        public int Score { get; set; }
+        public Dictionary<string, int> Reasons { get; set; }
+
+        public PlusPlusThing()
+        {
+            Reasons = new Dictionary<string, int>();
+        }
+    }
+}
