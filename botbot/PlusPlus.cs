51038a916069845b6f3e72aec37d8f0d011f14f1
Sanders Lauture <golf1052@msn.com>
2016-09-24 11:27:04 -0400

    More ++ stuff

--- botbot/PlusPlus.cs
+++ botbot/PlusPlus.cs
@@ -14,7 +14,25 @@ namespace botbot
             string pattern = "^([\\s\\w'@.\\-:\u3040-\u30FF\uFF01-\uFF60\u4E00-\u9FA0]*)\\s*(\\+\\+|--|—)(?:\\s+(?:for|because|cause|cuz|as)\\s+(.+))?$";
             Regex regex = new Regex(pattern);
             Match match = regex.Match(message);
-            return true;
+            if (match.Success)
+            {
+                string name = match.Groups[1].Value;
+                string vote = match.Groups[2].Value;
+                string reason = match.Groups[3].Value;
+                reason = reason.Trim().ToLower();
+                if (!string.IsNullOrEmpty(name))
+                {
+                    if (name[0] == ':')
+                    {
+                        name = Regex.Replace(name, "(^\\s*@)|([,\\s]*$)", "").Trim().ToLower();
+                    }
+                    else
+                    {
+                        name = Regex.Replace(name, "(^\\s*@)|([,:\\s]*$)", "").Trim().ToLower();
+                    }
+                }
+            }
+            return match.Success;
         }
     }
 }
---
+++ regex.md
@@ -0,0 +1,3 @@
+^([\s\w'@.\-:\u3040-\u30FF\uFF01-\uFF60\u4E00-\u9FA0]*)\s*(\+\+|--|—)(?:\s+(?:for|because|cause|cuz|as)\s+(.+))?$
+
+(?:erase )([\s\w'@.-:\u3040-\u30FF\uFF01-\uFF60\u4E00-\u9FA0]*)(?:\s+(?:for|because|cause|cuz)\s+(.+))?$
