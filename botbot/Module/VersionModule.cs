b12cbb3cdc234fc3ccb992c555eea2a423cc9e5c
Sanders Lauture <golf1052@msn.com>
2021-01-27 23:05:06 -0800

    44.0.0

--- botbot/Module/VersionModule.cs
+++ botbot/Module/VersionModule.cs
@@ -5,7 +5,7 @@ namespace botbot.Module
 {
     public class VersionModule : IMessageModule
     {
-        private const string Version = "43.1.0";
+        private const string Version = "44.0.0";
 
         public Task<ModuleResponse> Handle(string text, string userId, string channel)
         {
