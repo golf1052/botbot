351b539d20697590f9d1989a58c4433f80b47c54
Sanders Lauture <golf1052@msn.com>
2021-06-29 09:58:58 -0700

    Better pick HN item
    Algorithm is now this
    - Search on URL
    - If results search on title
    - Combine both URL and title results
    - Dedupe on date, post with the most points on a day remains
    - If there's only one item return that
    - Finally sort by date, descending, and return the first one

--- botbot/HackerNewsApi.cs
+++ botbot/HackerNewsApi.cs
@@ -20,24 +20,35 @@ namespace botbot
         {
             this.httpClient = httpClient;
         }
+
         public async Task<SearchItem?> Search(string url)
         {
-            HttpResponseMessage response = await httpClient.GetAsync($"http://hn.algolia.com/api/v1/search?query={url}&restrictSearchableAttributes=url");
-            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            List<SearchItem> hitItemsUrls = await Search(url, true);
+            
+            if (hitItemsUrls.Count == 0)
             {
                 return null;
             }
-            JObject responseObject = JObject.Parse(await response.Content.ReadAsStringAsync());
-            JArray? hits = (JArray?)responseObject["hits"];
-            if (hits == null || hits.Count == 0)
-            {
-                return null;
-            }
+
+            List<SearchItem> hitItemsTitles = await Search(hitItemsUrls[0].Title!, false);
+
             List<SearchItem> hitItems = new List<SearchItem>();
-            foreach (var hit in hits)
+            hitItems.AddRange(hitItemsUrls);
+            for (int i = 0; i < hitItemsTitles.Count; i++)
             {
-                hitItems.Add(JsonConvert.DeserializeObject<SearchItem>(hit.ToString()));
+                foreach (var hitItem in hitItems)
+                {
+                    if (hitItem.Id == hitItemsTitles[i].Id)
+                    {
+                        hitItemsTitles.RemoveAt(i);
+                        i--;
+                        break;
+                    }
+                }
             }
+            hitItems.AddRange(hitItemsTitles);
+
+            DedupeOnSameDate(hitItems);
 
             // If there's only one item just return that
             if (hitItems.Count == 1)
@@ -58,6 +69,60 @@ namespace botbot
                 return b.CreatedAt.CompareTo(a.CreatedAt);
             });
             return hitItems[0];
+        }
+
+        private async Task<List<SearchItem>> Search(string query, bool onlyUrls)
+        {
+            string searchUrl = $"http://hn.algolia.com/api/v1/search?query={query}";
+            if (onlyUrls)
+            {
+                searchUrl += "&restrictSearchableAttributes=url";
+            }
+
+            HttpResponseMessage response = await httpClient.GetAsync(searchUrl);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                return new List<SearchItem>();
+            }
+
+            JObject responseObject = JObject.Parse(await response.Content.ReadAsStringAsync());
+            JArray? hits = (JArray?)responseObject["hits"];
+            if (hits == null || hits.Count == 0)
+            {
+                return new List<SearchItem>();
+            }
+
+            List<SearchItem> hitItems = new List<SearchItem>();
+            foreach (var hit in hits)
+            {
+                hitItems.Add(JsonConvert.DeserializeObject<SearchItem>(hit.ToString()));
+            }
+            return hitItems;
+        }
+
+        private void DedupeOnSameDate(List<SearchItem> hitItems)
+        {
+            // This will be way easier once DateOnly lands
+            // https://devblogs.microsoft.com/dotnet/date-time-and-time-zone-enhancements-in-net-6/
+            Dictionary<DateTime, SearchItem> dates = new Dictionary<DateTime, SearchItem>();
+            foreach (var hitItem in hitItems)
+            {
+                DateTime date = new DateTime(hitItem.CreatedAt.Year, hitItem.CreatedAt.Month, hitItem.CreatedAt.Day);
+                if (!dates.ContainsKey(date))
+                {
+                    dates.Add(date, hitItem);
+                }
+                else
+                {
+                    if (hitItem.Points > dates[date].Points)
+                    {
+                        dates[date] = hitItem;
+                    }
+                }
+            }
+
+            hitItems.Clear();
+            hitItems.AddRange(dates.Values);
         }
     }
 
