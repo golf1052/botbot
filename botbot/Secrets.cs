1fc975b44a99fc792f27883e3ee1e2145e9b5c27
Sanders Lauture <golf1052@msn.com>
2023-02-16 23:16:48 -0800

    Create Secrets.cs

---
+++ botbot/Secrets.cs
@@ -0,0 +1,28 @@
+namespace botbot
+{
+    public sealed class Secrets
+    {
+        public const string MongoConnectionString = "";
+        public const string SoundcloudClientId = "";
+        public const string SoundCloudClientSecret = "";
+        public const string SoundCloudPassword = "";
+        public const string SpotifyClientId = "";
+        public const string SpotifyClientSecret = "";
+        public const string SpotifyRedirectUrl = "";
+        public const string AlphaVantageApiKey = "";
+        public const string IEXPublishableToken = "";
+        public const string IEXSecretToken = "";
+        public const string IEXSandboxPublishableToken = "";
+        public const string IEXSandboxSecretToken = "";
+        public const string SpotifyNewReleasesClientId = "";
+        public const string SpotifyNewReleasesClientSecret = "";
+        public const ushort GPMServerPortNumber = 5000;
+        public const ushort HubotPortNumber = 6789;
+        public const string SpotifyDeduperClientId = "";
+        public const string SpotifyDeduperClientSecret = "";
+        public const string OpenAIOrganization = "";
+        public const string OpenAIApiKey = "";
+
+        public const string DiscordToken = "";
+    }
+}
