18158efd9ea7592165b6f5e528ab155065546fbb
zdhickman <zdhickman@gmail.com>
2023-02-19 13:18:24 -0800

    Use == for string comparison and don't split message in many places

--- botbot/Module/OpenAIModule.cs
+++ botbot/Module/OpenAIModule.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using OpenAI.GPT3.Managers;
 using OpenAI.GPT3;
 using OpenAI.GPT3.ObjectModels.RequestModels;
@@ -25,42 +24,29 @@ namespace botbot.Module
             encoder = Encoder.Get_Encoder();
         }
 
-        private string GetPrefix(string text) {
-            string[] splitText = text.Split(' ');
-            string command = splitText[1];
-            if (String.Equals(command, "eli5") ){
+        private string GetPrefix(string command)
+        {
+            if (command == "eli5"){
                 return "Explain like I'm 5 years old: ";
             }
 
             return "";
         }
 
-        private string GetPrompt(string text) {
-            string[] splitText = text.Split(' ');
-            if (splitText[2].StartsWith("temp")) {
-                return text.Substring(
-                    splitText[0].Length + splitText[1].Length + splitText[2].Length + 2
-                ).Trim();
-            }
-            return text.Substring(splitText[0].Length + splitText[1].Length + 1).Trim();
-        }
-
-        private float GetTemp(string text) {
-            string[] splitText = text.Split(' ');
-            if (splitText[2].StartsWith("temp")) {
-                string[] splitTemp = splitText[2].Split(':');
-                return float.Parse(splitTemp[1]);
-            }
-            return 1;
-        }
-
         public async Task<ModuleResponse> Handle(string text, string userId, string channel)
         {
             if (text.ToLower().StartsWith("botbot gpt") || text.ToLower().StartsWith("botbot eli5"))
             {
-                string prefix = GetPrefix(text);
-                string prompt = prefix + GetPrompt(text);
-                float temp = GetTemp(text);
+                string[] splitText = text.Split(' ');
+                string prefix = GetPrefix(splitText[1]);
+                string prompt = string.Join(" ", splitText.Skip(2).Take(splitText.Length - 1));
+                float temp = 1;
+                if (splitText[2].StartsWith("temp"))
+                {
+                    temp = float.Parse(splitText[2].Split(':')[1]);
+                    prompt = string.Join(" ", splitText.Skip(3).Take(splitText.Length - 1));
+                }
+                prompt = prefix + prompt;
 
                 // Encode our text first so we know how many remaining tokens we have
                 var tokens = encoder.Encode(prompt);
