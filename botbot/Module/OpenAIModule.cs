4699fe7f9252f2e6ea6615dcbfa1f0db0addb113
zdhickman <zdhickman@gmail.com>
2023-02-19 11:43:56 -0800

    Add support for `eli5` OpenAI command

--- botbot/Module/OpenAIModule.cs
+++ botbot/Module/OpenAIModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using OpenAI.GPT3.Managers;
 using OpenAI.GPT3;
 using OpenAI.GPT3.ObjectModels.RequestModels;
@@ -24,26 +25,49 @@ namespace botbot.Module
             encoder = Encoder.Get_Encoder();
         }
 
+        private string GetPrefix(string text) {
+            string[] splitText = text.Split(' ');
+            string command = splitText[1];
+            if (String.Equals(command, "eli5") ){
+                return "Explain like I'm 5 years old: ";
+            }
+
+            return "";
+        }
+
+        private string GetPrompt(string text) {
+            string[] splitText = text.Split(' ');
+            if (splitText[2].StartsWith("temp")) {
+                return text.Substring(
+                    splitText[0].Length + splitText[1].Length + splitText[2].Length + 2
+                ).Trim();
+            }
+            return text.Substring(splitText[0].Length + splitText[1].Length + 1).Trim();
+        }
+
+        private float GetTemp(string text) {
+            string[] splitText = text.Split(' ');
+            if (splitText[2].StartsWith("temp")) {
+                string[] splitTemp = splitText[2].Split(':');
+                return float.Parse(splitTemp[1]);
+            }
+            return 1;
+        }
+
         public async Task<ModuleResponse> Handle(string text, string userId, string channel)
         {
-            if (text.ToLower().StartsWith("botbot gpt"))
+            if (text.ToLower().StartsWith("botbot gpt") || text.ToLower().StartsWith("botbot eli5"))
             {
-                text = text.Substring(10).Trim();
-                float temp = 1;
-                if (text.StartsWith("temp"))
-                {
-                    string[] splitText = text.Split(' ');
-                    string[] splitTemp = splitText[0].Split(':');
-                    temp = float.Parse(splitTemp[1]);
-                    text = text.Substring(splitText[0].Length).Trim();
-                }
+                string prefix = GetPrefix(text);
+                string prompt = prefix + GetPrompt(text);
+                float temp = GetTemp(text);
 
                 // Encode our text first so we know how many remaining tokens we have
-                var tokens = encoder.Encode(text);
+                var tokens = encoder.Encode(prompt);
 
                 var completionResult = await openAIService.Completions.CreateCompletion(new CompletionCreateRequest()
                 {
-                    Prompt = text,
+                    Prompt = prompt,
                     Model = Models.TextDavinciV3,
                     Temperature = temp,
                     MaxTokens = MaxTokens - tokens.Count
