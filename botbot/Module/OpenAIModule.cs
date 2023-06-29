d1e3675a6c725c539d0d514784dc77dae6786b23
Sanders Lauture <golf1052@msn.com>
2023-06-28 23:48:16 -0700

    Fix tokenizer counting
    The tokenizer included in Betalgo uses an older tokenizer which is incorrect for gpt-3.5 and higher. To fix this I'm now using Tiktoken which is currently the fastest port of the official Python version of the library.
    
    Also OpenAI adds additional tokens which must be accounted for when using the Chat Completions API. This is now accounted for.

--- botbot/Module/OpenAIModule.cs
+++ botbot/Module/OpenAIModule.cs
@@ -4,15 +4,16 @@ using System.Threading.Tasks;
 using OpenAI;
 using OpenAI.Managers;
 using OpenAI.ObjectModels.RequestModels;
-using OpenAI.Tokenizer.GPT3;
 
 namespace botbot.Module
 {
     public class OpenAIModule : IMessageModule
     {
+        private const string EncodingName = "cl100k_base";
+
         private const int MaxTokens = 4096;
         private readonly OpenAIService openAIService;
-        //private readonly Encoder encoder;
+        private Tiktoken.Encoding encoder;
 
         public OpenAIModule()
         {
@@ -21,7 +22,7 @@ namespace botbot.Module
                 ApiKey = Secrets.OpenAIApiKey,
                 Organization = Secrets.OpenAIOrganization
             });
-            //encoder = Encoder.Get_Encoder();
+            encoder = Tiktoken.Encoding.Get(EncodingName);
         }
 
         public async Task<ModuleResponse> Handle(string text, string userId, string channel)
@@ -38,9 +39,6 @@ namespace botbot.Module
                     prompt = string.Join(" ", splitText.Skip(3).Take(splitText.Length - 1));
                 }
 
-                // Encode our text first so we know how many remaining tokens we have
-                var tokens = TokenizerGpt3.Encode(prefix + prompt);
-
                 List<ChatMessage> messages = new List<ChatMessage>();
                 if (!string.IsNullOrWhiteSpace(prefix))
                 {
@@ -53,7 +51,7 @@ namespace botbot.Module
                     Messages = messages,
                     Model = "gpt-3.5-turbo-0613",
                     Temperature = temp,
-                    MaxTokens = MaxTokens - tokens.Count()
+                    MaxTokens = MaxTokens - GetTokenCount(messages)
                 });
                 
                 if (completionResult.Successful)
@@ -101,6 +99,44 @@ namespace botbot.Module
                 }
             }
             return new ModuleResponse();
+        }
+
+        private int GetTokenCount(List<ChatMessage> messages)
+        {
+            // Using external tokenizer currently because Betalgo.OpenAI tokenizer (TokenizerGpt3) uses p50k_base
+            // which is incorrect for gpt-3.5 and higher.
+            // Using Tiktoken over TiktokenSharp or SharpToken because it is the fastest tokenizer as of 2023-06-28
+            // BenchmarkDotNet=v0.13.5, OS=Windows 11 (10.0.22621.1926/22H2/2022Update/SunValley2)
+            // AMD Ryzen 9 5900X, 1 CPU, 24 logical and 12 physical cores
+            //.NET SDK = 7.0.304
+            //  [Host]     : .NET 7.0.7(7.0.723.27404), X64 RyuJIT AVX2
+            //  DefaultJob : .NET 7.0.7(7.0.723.27404), X64 RyuJIT AVX2
+            //| Method | Mean | Error | StdDev |
+            //| -------------------- | ------------:| ---------:| ---------:|
+            //| TikTokenSharpEncode (v1.0.6)  | 247.72 us   | 0.997 us | 0.933 us |
+            //| SharpTokenEncode (v1.0.30)    | 1,390.72 us | 6.996 us | 6.544 us |
+            //| TikTokenEncode (v1.1.0)       | 92.29 us    | 0.289 us | 0.271 us |
+
+            // Encode our messages so we know how many remaining tokens we have
+            // This is from https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+            // because OpenAI adds stuff which gets counted in the final token count
+
+            const int TokensPerMessage = 4;
+            const int TokensPerName = 1;
+            const int ReplyTokens = 3;
+
+            int numTokens = 0;
+            foreach (var message in messages)
+            {
+                numTokens += TokensPerMessage;
+                if (!string.IsNullOrWhiteSpace(message.Name))
+                {
+                    numTokens += TokensPerName;
+                }
+                numTokens += encoder.CountTokens(message.Content);
+            }
+            numTokens += ReplyTokens;
+            return numTokens;
         }
 
         private string GetPrefix(string command)
